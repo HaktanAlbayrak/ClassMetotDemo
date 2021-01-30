1e9b90873f787175c819781c53c989fdd119bb9e
HaktanAlbayrak <78109456+HaktanAlbayrak@users.noreply.github.com>
2021-01-30 22:02:22 +0300

    Ödev3

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public  void Ekle(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri başarıyla Eklendi " + musteri.Ad + " " + musteri.Soyad);
+            
+        }
+        public void Sil(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri başarıyla Silindi " + musteri.Ad + " " + musteri.Soyad);
+            
+        }
+        
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Id = 1;
+            musteri1.Ad = "Haktan";
+            musteri1.Soyad = "Albayrak";
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Id = 2;
+            musteri2.Ad = "Melike";
+            musteri2.Soyad = "İrgin";
+
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2 };
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Ekle(musteri1);
+            musteriManager.Ekle(musteri2);
+
+            musteriManager.Sil(musteri1);
+            musteriManager.Sil(musteri2);
+
+            foreach  (var Musteri in musteriler )
+            {
+                Console.WriteLine("Müşteri AD :" + Musteri.Ad);
+                Console.WriteLine("Müşteri Soyad :" + Musteri.Soyad);
+            }
+        }
+    }
+}
